0e0c986bdc31c202117d198af2a934922092ee81
Maksym Voloshko <maksimvoloshko02@gmail.com>
2023-02-04 01:40:18 +0200

    #4 Create data package
    Created data package and obstacle factory.
    Also added di for this package

---
+++ src/jvmMain/kotlin/data/factory/ObstaclesFactory.kt
@@ -0,0 +1,18 @@
+package data.factory
+
+import domain.model.Obstacle
+import domain.model.Point
+
+class ObstaclesFactory {
+    fun get(): List<Obstacle> = listOf(
+        Obstacle(
+            listOf(
+                Point(10, 10),
+                Point(10, 20),
+                Point(20, 20),
+                Point(20, 10),
+                Point(10, 10)
+            )
+        )
+    )
+}
