65f01c18ffdf3fa9e1604949ac5b260b086f8ac8
Maksym Voloshko <maksimvoloshko02@gmail.com>
2023-02-04 01:38:26 +0200

    Fixed comments on the code

--- src/jvmMain/kotlin/data/factory/ObstaclesFactory.kt
+++ src/jvmMain/kotlin/data/factory/ObstaclesFactory.kt
@@ -4,17 +4,15 @@ import domain.model.Obstacle
 import domain.model.Point
 
 class ObstaclesFactory {
-    fun get(): List<Obstacle> {
-        return listOf(
-            Obstacle(
-                listOf(
-                    Point(10, 10),
-                    Point(10, 20),
-                    Point(20, 20),
-                    Point(20, 10),
-                    Point(10, 10)
-                )
+    fun get(): List<Obstacle> = listOf(
+        Obstacle(
+            listOf(
+                Point(10, 10),
+                Point(10, 20),
+                Point(20, 20),
+                Point(20, 10),
+                Point(10, 10)
             )
         )
-    }
+    )
 }
