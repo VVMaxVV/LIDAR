b99294cd7780d5b4486adac8d84a42956a3d825a
Maksym Voloshko <maksimvoloshko02@gmail.com>
2023-03-09 00:35:56 +0200

    #17 Refactor including koin modules
    Refactored including koin modules

--- presenter/src/jvmMain/kotlin/di/viewModelModule.kt
+++ presenter/src/jvmMain/kotlin/di/viewModelModule.kt
@@ -4,7 +4,7 @@ import org.koin.dsl.module
 import viewModel.ControllerMovementsViewModel
 import viewModel.RayCalculationViewModel
 
-val viewModelModule = module {
+internal val viewModelModule = module {
     single { RayCalculationViewModel(get(), get(), get(), get(), get()) }
     single { ControllerMovementsViewModel(get()) }
 }
