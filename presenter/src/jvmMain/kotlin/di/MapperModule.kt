be62b85082e289dee741ffd9e42cfea44ce29e2a
Maksym Voloshko <maksimvoloshko02@gmail.com>
2023-03-09 01:14:44 +0200

    #18 Setup and configure ktlint
    Installed `ktlint` plugin

--- presenter/src/jvmMain/kotlin/di/MapperModule.kt
+++ presenter/src/jvmMain/kotlin/di/MapperModule.kt
@@ -1,8 +1,12 @@
 package di
 
 import mapper.DistanceToCollisionMapper
+import mapper.PointMapper
+import mapper.RayMapper
 import org.koin.dsl.module
 
 internal val mapperModule = module {
-    single { DistanceToCollisionMapper() }
+    single { RayMapper() }
+    single { PointMapper() }
+    single { DistanceToCollisionMapper(get()) }
 }
--- presenter/src/jvmMain/kotlin/di/ViewModelModule.kt
+++ presenter/src/jvmMain/kotlin/di/ViewModelModule.kt
@@ -1,12 +1,10 @@
 package di
 
 import org.koin.dsl.module
-import viewModel.CanvasLidarViewModel
 import viewModel.ControllerMovementsViewModel
 import viewModel.RayCalculationViewModel
 
 internal val viewModelModule = module {
-    single { RayCalculationViewModel(get(), get(), get(), get()) }
-    single { ControllerMovementsViewModel(get(), get()) }
-    single { CanvasLidarViewModel() }
+    single { RayCalculationViewModel(get(), get(), get(), get(), get()) }
+    single { ControllerMovementsViewModel(get()) }
 }
