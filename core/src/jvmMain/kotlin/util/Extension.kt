b1d222b755ed3475cf0f44588f25a850b5c473d4
Maksym Voloshko <maksimvoloshko02@gmail.com>
2023-04-19 04:41:00 +0300

    Created chunk handling for deeper division into objects with which intersection is possible

--- core/src/jvmMain/kotlin/util/Extension.kt
+++ core/src/jvmMain/kotlin/util/Extension.kt
@@ -1,7 +1,10 @@
 package util
 
 import androidx.compose.ui.geometry.Offset
+import model.Point
 
 fun Offset.scale(scaleX: Number = 1, scaleY: Number = 1) = Offset(this.x * scaleX, this.y * scaleY)
 
 fun Offset.move(offsetX: Number = 0, offsetY: Number = 0) = Offset(this.x + offsetX, this.y + offsetY)
+
+fun Offset.toPoint() = Point(this.x, this.y)
