f78f29696f6b560e00f32536bb3369a1fcc9d89f
Maksym Voloshko <maksimvoloshko02@gmail.com>
2023-04-19 04:41:00 +0300

    Created chunk handling for deeper division into objects with which intersection is possible

--- data/src/jvmMain/kotlin/repository/ObstaclesRepositoryImpl.kt
+++ data/src/jvmMain/kotlin/repository/ObstaclesRepositoryImpl.kt
@@ -2,7 +2,11 @@ package repository
 
 import factory.ObstaclesFactory
 import model.Obstacle
+import model.Point
 
 internal class ObstaclesRepositoryImpl(private val obstaclesFactory: ObstaclesFactory) : ObstaclesRepository {
-    override fun getAllObstacles(): List<Obstacle> = obstaclesFactory.get()
+    override fun getAllObstacles(): List<Obstacle> = obstaclesFactory.getAll()
+
+    override fun getLinesWithinPoints(firstPoint: Point, secondPoint: Point) =
+        obstaclesFactory.get(firstPoint, secondPoint)
 }
